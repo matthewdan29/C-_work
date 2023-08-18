055ce90d4dfd8db28a061ede7dacf42ce7c904fc
Matthew K Daniels <140857904+matthewdan29@users.noreply.github.com>
2023-08-17 19:35:08 -0500

    Update hellowrld.cpp
    The first copy was never supposed to be published

--- C++/2Chapter_Tour_of_basics/hellowrld.cpp
+++ C++/2Chapter_Tour_of_basics/hellowrld.cpp
@@ -2,5 +2,5 @@
 
 int main()
 {
-	std::cout<< "Hello, World Matthew Got a BBC with a big mushroom head!\n"; /* Hey this is a fist copy if I end up uploading this version on my github I was being sloopy Please let me know.*/
+	std::cout<< "Hello, World Matthew \n"; /* Hey this is a fist copy if I end up uploading this version on my github I was being sloopy Please let me know.*/
 }
